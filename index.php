9b886b2d1b0f49dc6c4100619e7a27429050d266
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2017-11-22 17:08:11 +1100

    add composer

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.19.5790cb/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+$dirname = dirname(__FILE__);
+if (file_exists($dirname . '/vendor/autoload.php')){
+    require_once($dirname . '/vendor/autoload.php');
+}
+if (file_exists($dirname . '/vendor/yiisoft/yii/framework/yii.php')) {
+    $yii = $dirname . '/vendor/yiisoft/yii/framework/yii.php';
+} else {
+    $yii = $dirname . '/protected/yii/framework/yii.php';
+}$config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
