fd8cc47b89b05cd6dcbd557a391ad93ce4e3bea1
Zhaolian-Zhou <zhaolian.zhou@fivium.com.au>
2017-12-05 17:04:03 +1100

    minor change

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -31,10 +31,10 @@
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'Author', 'url'=> array('author/index')),
-				array('label'=>'Chronology', 'url'=>array('chronology/index')),
+				array('label'=>'', 'url'=>array('chronology/index')),
 				array('label'=>'Toponymy', 'url'=>array('toponymy/index')),
 				array('label'=>'Relations', 'url'=> array('relations/index')),
-				array('label'=>'Literature','url'=>array('literature/index')),
+				array('label'=>'文學作品','url'=>array('literature/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
